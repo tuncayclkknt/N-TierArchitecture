8a01a94609282533abdb18b8928285ccabd61768
tuncayclkknt <tuncayclkknt@gmail.com>
2024-10-29 18:52:43 +0300

    Add project files.

---
+++ CSharkEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharkEgitimKampi301.EntityLayer.Concrete
+{
+    public class Category
+    {
+        public int CatagoryId { get; }//primary key and autoincreasing
+        public string CatagoryName { get; set; }
+        public bool CatagoryStatus { get; set; }
+
+    }
+}
+/*
+ Fielad - Variable - Property
+
+int x; ---> Field
+public int x {get; set;} --> Property
+int x; (in an method) --> Variable
+
+ */
---
+++ CSharkEgitimKampi301.EntityLayer/Concrete/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharkEgitimKampi301.EntityLayer.Concrete
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int ProductStock { get; set; }
+        public decimal ProductPrice { get; set; }
+        public string PrdouctDescription { get; set; }
+    }
+}
