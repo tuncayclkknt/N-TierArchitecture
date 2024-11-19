58e4f927a867495b401c8b5bb9117505d4cd5eb3
tuncayclkknt <tuncayclkknt@gmail.com>
2024-11-19 22:20:32 +0300

    New frame design added for locaition

---
+++ CSharpEgitimKampi301.EFProject/FrmLocation.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity.Infrastructure;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpEgitimKampi301.EFProject
+{
+    public partial class FrmLocation : Form
+    {
+        public FrmLocation()
+        {
+            InitializeComponent();
+        }
+
+        EgitimKampiEFTravelDbEntities db = new EgitimKampiEFTravelDbEntities();
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            var values = db.TblLocation.Select(location => new
+            {
+                location.LocationId,
+                location.LocationCity,
+                location.LocationCountry,
+                location.LocationCapacity,
+                location.LocationPrice,
+                location.LocationDayNight
+            }).ToList();
+
+            dataGridView1.DataSource = values;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void FrmLocation_Load(object sender, EventArgs e)
+        {
+            var values = db.TblGuide.Select(x => new
+            {
+                FullName = x.GuideName + " " + x.GuideSurname,
+                x.GuideId
+            }).ToList();
+
+            cmbGuide.DisplayMember = "FullName";
+            cmbGuide.ValueMember = "GuideId";
+            cmbGuide.DataSource = values;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            TblLocation location = new TblLocation();
+            location.LocationCapacity = byte.Parse(nudCapacity.Value.ToString());
+            location.LocationCity = txtCity.Text;
+            location.LocationPrice = decimal.Parse(txtPrice.Text);
+            location.LocationCountry = txtCountry.Text;
+            location.LocationDayNight = txtDayNight.Text;
+            location.GuideId = int.Parse(cmbGuide.SelectedValue.ToString());
+            db.TblLocation.Add(location);
+            db.SaveChanges();
+            MessageBox.Show("Addition is successfull.");
+
+            btnList_Click(sender,e);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var deletedValue = db.TblLocation.Find(id);
+            db.TblLocation.Remove(deletedValue);
+            db.SaveChanges();
+            MessageBox.Show("Deletion is successfull.");
+
+            btnList_Click(sender, e);
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtId.Text);
+            var updatedValue = db.TblLocation.Find(id);
+            updatedValue.LocationCapacity = byte.Parse(nudCapacity.Value.ToString());
+            updatedValue.LocationCity = txtCity.Text;
+            updatedValue.LocationPrice = decimal.Parse(txtPrice.Text);
+            updatedValue.LocationCountry = txtCountry.Text;
+            updatedValue.LocationDayNight = txtDayNight.Text;
+            updatedValue.GuideId = int.Parse(cmbGuide.SelectedValue.ToString());
+            db.SaveChanges();
+            MessageBox.Show("Update is successfull.");
+
+            btnList_Click(sender, e);
+
+        }
+    }
+}
